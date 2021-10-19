dcd0bdc973c6f9988a7087edcb5d357034a30174
Josue Castro Chaves <36125321+Kr4K1sH@users.noreply.github.com>
2021-10-18 18:13:45 -0600

    Add files via upload

---
+++ Turisteando/BD/Imagenes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Turisteando.BD
+{
+    public class Imagenes
+    {
+        public int idIma { set; get; }
+        public string Lugar { set; get; }
+
+        public string nombre { set; get; }
+
+        public byte[] img { set; get; }
+
+        public List<Imagenes> SeleccionarTodos()
+        {
+
+            Database db = DataBaseFactory.CreateDatabase("ProyectoConnectionString");
+
+            SqlCommand comando = new SqlCommand("Select * from Imagenes");
+            comando.CommandType = CommandType.Text;
+            comando.Connection = db.Conexion;
+            List<Imagenes> imagens = new List<Imagenes>();
+            SqlDataReader lee = comando.ExecuteReader();
+            while (lee.Read())
+            {
+                Imagenes v = new Imagenes();
+                v.idIma = Convert.ToInt32(lee["ID"].ToString());
+                v.Lugar = (lee["Lugar"].ToString());
+                v.nombre = (lee["NombreImagen"].ToString());
+                v.img = (byte[])lee["Imagen"];
+                imagens.Add(v);
+            }
+
+            return imagens;
+        }
+        public string URL(byte[] bytes)
+        {
+            string URL = "";
+            string base64 = "";
+            base64 = Convert.ToBase64String(bytes);
+            URL += "data:Image/jpg;base64," + base64;
+            return URL;
+        }
+    }
+
+}
---
+++ Turisteando/BD/Informacion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Turisteando.BD;
+
+namespace Turisteando.BD
+{
+
+    public class Informacion
+    {
+        public int ID { set; get; }
+        public string lugar { set; get; }
+        public string Telefono { set; get; }
+
+        public string Precio { set; get; }
+        public string informacion { set; get; }
+
+        public List<Informacion> SeleccionarTodos()
+        {
+
+            Database db = DataBaseFactory.CreateDatabase("ProyectoConnectionString");
+
+            SqlCommand comando = new SqlCommand("Select * from Informacion");
+            comando.CommandType = CommandType.Text;
+            comando.Connection = db.Conexion;
+            List<Informacion> Info = new List<Informacion>();
+            SqlDataReader lee = comando.ExecuteReader();
+            while (lee.Read())
+            {
+                Informacion v = new Informacion();
+                v.ID = Convert.ToInt32(lee["ID"].ToString());
+                v.lugar = lee["Lugar"].ToString();
+                v.Telefono = lee["Telefonos"].ToString();
+                v.Precio = lee["Precio"].ToString();
+                v.informacion = lee["Informacion"].ToString();
+                Info.Add(v);
+            }
+
+            return Info;
+        }
+    }
+}
